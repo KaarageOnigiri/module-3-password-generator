c0302086e9853d4286cb0df17c60c77f5556a553
Villy Yeoh <yeohvilly@gmail.com>
2023-09-01 12:12:39 -0400

    changed the 'if' statement to make it more readable and work as intended

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-const passwordText = document.querySelector("#password");
-// this one might not be used
 
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const number = "0123456789";
-const symbol = "~!@#$%^&*()_+{}|-=[]<>/";
+function generatePassword() {
+  // testing
+  console.log("You ran the function 'generatePassword()'")
 
+  // 1. write prompts
+  // 2. validate the inputs
+  // 3. generate password based on criteria
+  // 4. display password to the page (already done)
 
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  const number = "0123456789";
+  const symbol = "~!@#$%^&*()_+{}|-=[]<>/";
 
-// Write password to the #password input
-function writePassword() {
-  
   var length = prompt("Please enter the length of your password:");
 
   while (length < 8 || length > 128) {
@@ -23,44 +25,49 @@ function writePassword() {
   var lowerCasePermission = prompt("Would you like lower case to be included? (Type 'yes' or 'no')");
   var numberPermission = prompt("Would you like numbers to be included? (Type 'yes' or 'no')");
   var symbolPermission = prompt("Would you like symbols to be included? (Type 'yes' or 'no')");
-
   
+  console.log(upperCasePermission, lowerCasePermission, numberPermission, symbolPermission);
   
-  function generatePassword() {
-    var password = "";
-    var combined;
-
-    if (upperCasePermission == "yes" || "Yes" || "YES") {
-      password += upperCase[Math.floor(Math.random()*upperCase.length)];
-      combined += upperCase;
-    }
-    if (lowerCasePermission == "yes" || "Yes" || "YES") {
-      password += lowerCase[Math.floor(Math.random()*lowerCase.length)];
-      combined += lowerCase;
-    }
-    if (numberPermission == "yes" || "Yes" || "YES") {
-      password += number[Math.floor(Math.random()*number.length)];
-      combined += number;
-    }
-    if (symbolPermission == "yes" || "Yes" || "YES") {
-      password += symbol[Math.floor(Math.random()*symbol.length)];
-      combined += symbol;
-    }
-    
-
-    while (password.length < length) {
-      password += combined[Math.floor(Math.random()*combined.length)];
-    }
+  var password = "";
+  var combined;
+  
+  if (upperCasePermission.toLowerCase() === "yes") {
+    password += upperCase[Math.floor(Math.random()*upperCase.length)];
+    combined += upperCase;
+    // testing
+    console.log("upper case");
+  }
+  if (lowerCasePermission.toLowerCase() === "yes") {
+    password += lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    combined += lowerCase;
+    console.log("lower case");
+  }
+  if (numberPermission.toLowerCase() === "yes") {
+    password += number[Math.floor(Math.random()*number.length)];
+    combined += number;
+    //testing
+    console.log("number");
+  }
+  if (symbolPermission.toLowerCase() === "yes") {
+    password += symbol[Math.floor(Math.random()*symbol.length)];
+    combined += symbol;
+    // testing
+    console.log("symbol");
+  }
+  
+  while (password.length < length) {
+    password += combined[Math.floor(Math.random()*combined.length)];
   }
 
-  passwordText.value = password;
-  // wrong
-  console.log(passwordText)
+  return password;
+}
 
-  // I don't get this line
+// Write password to the #password input
+function writePassword() {
   var password = generatePassword();
 
-   
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
